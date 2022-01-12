22898b7098414579b83b326c25e6bdac04f8ac15
thadakaajay <thadakaajay65@gmail.com>
2022-01-12 23:13:28 +0530

    This is my first pogram

---
+++ src/Myclass.java
@@ -0,0 +1,13 @@
+
+public class myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
